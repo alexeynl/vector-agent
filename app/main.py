5ef09c1bd67899cbe9a3873ae5d346778ec20cbf
AlexeyNL <alexeynl@bk.ru>
2025-05-19 23:11:12 +0300

    refactor to class, add config

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ import app.utils as f
 
 appl = FastAPI()
 
+# todo: add read path from command line
+va = f.VectorAgent("/mnt/d/dev/github/vector-agent/app/config.yaml")
+
 @appl.get("/validate/{branch}")
 def api_vector_validate_config_branch(branch: str):
-    return f.vector_validate_config_branch(f.repo, branch)
+    return va.validate_config_branch(branch)
--- app/utils.py
+++ app/utils.py
@@ -1,55 +1,115 @@
 import subprocess
 import tempfile
 import os
+import yaml
+
+
+default_vector_bin_path = "/usr/bin/vector"
+default_gitsync_bin_path = "/usr/bin/git-sync"
 
 vector_bin_path = "/opt/vector/bin/vector"
 gitsync_bin_path = "/opt/git-sync/bin/git-sync"
+
 config_branch = "test-branch"
+config_subdirs = ["test"]
 repo = "https://github.com/alexeynl/vector-configs.git"
 root_vrl_path_env_name = "VECTOR_CONFIG_PATH"
 env_files = ["/etc/vector/.env"]
+ssh_key_path = ""
+ssh_known_hosts_path = ""
 
-def vector_validate_config_branch(repo, branch):
-    envs = load_envs(env_files)
-    print(envs)
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        gitsync_root =  os.path.join(tmpdirname, "root")
-        gitsync_link = os.path.join(tmpdirname, "configs")
-        #envs = envs | {root_vrl_path_env_name: gitsync_link}
-        print(envs)
-        sync_result = sync_branch(repo, branch, gitsync_root, gitsync_link)
-        result = {}
-        status = "ok"
-        if sync_result["status"] == "fail":
-            status = "fail"
-            result["reason"] = sync_result["reason"]
-        else:
-            p = subprocess.run([vector_bin_path, "validate", "-C", gitsync_link], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=envs)
-            if p.returncode != 0:
+class VectorAgent:
+    def __init__(self, config_path):
+        self._config_path = config_path
+        self._vector_bin_path = default_vector_bin_path
+        self._gitsync_bin_path = default_gitsync_bin_path
+        self._load_config(config_path)
+        # todo: add attribute validation
+        if not hasattr(self, "_config_subdirs"):
+            self._config_subdirs = []
+        
+    def _load_config(self, config_path: str):
+        with open(config_path, 'r') as f:
+            data = yaml.safe_load(f)
+            try:
+                self._vector_bin_path = data["vector"]["bin_path"]
+            except KeyError:
+                pass
+
+            try:
+                self._gitsync_bin_path = data["git-sync"]["bin_path"]
+            except KeyError:
+                pass
+            
+            try:
+                self._repo_url = data["vector-agent"]["repo"]["url"]
+            except KeyError:
+                pass
+
+            try:
+                self._ssh_key_path = data["vector-agent"]["repo"]["ssh_key_path"]
+            except KeyError:
+                pass
+
+            try:
+                self._ssh_known_hosts_path = data["vector-agent"]["repo"]["ssh_known_hosts_path"]
+            except KeyError:
+                pass
+
+            try:
+                self._env_files = data["vector-agent"]["env_files"]
+            except KeyError:
+                pass
+
+            try:
+                self._root_vrl_path_env_name = data["vector-agent"]["root_vrl_path_env_name"]
+            except KeyError:
+                pass
+
+            try:
+                self._config_subdirs = data["vector-agent"]["config_subdirs"]
+            except KeyError:
+                pass
+
+    def validate_config_branch(self, branch: str):
+        envs = load_envs(self._env_files)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            gitsync_root = os.path.join(tmpdirname, "root")
+            gitsync_link = os.path.join(tmpdirname, "configs")
+            envs = envs | {self._root_vrl_path_env_name: gitsync_link}
+            sync_result = self._sync_branch(self._repo_url, branch, gitsync_root, gitsync_link)
+            result = {}
+            status = "ok"
+            if sync_result["status"] == "fail":
                 status = "fail"
-                result["reason"] = "Incorrect config"
-            result["output"] = p.stdout
-        result["status"] = status
+                result["reason"] = sync_result["reason"]
+            else:
+                if len(self._config_subdirs) == 0:
+                    cmd = [vector_bin_path, "validate", "-C", gitsync_link]
+                else:
+                    config_dirs_str = ",".join([os.path.join(gitsync_link, subdir) for subdir in config_subdirs])
+                    cmd = [vector_bin_path, "validate", "-C", config_dirs_str]
+                print("Validation command: {}".format(" ".join(cmd)))
+                p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=envs)
+                if p.returncode != 0:
+                    status = "fail"
+                    result["reason"] = "Incorrect config"
+                result["output"] = p.stdout
+            result["status"] = status
+            return result
+
+    def _sync_branch(self, repo_url: str, branch: str, gitsync_root_path: str, git_sync_link_path: str):
+        cmd = [gitsync_bin_path, "--repo", repo_url, "--ref", branch, "--root", gitsync_root_path, "--link", git_sync_link_path, "--one-time"]
+        print("Sync command: {}".format(" ".join(cmd)))
+        p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = {}
+        result_status = "ok"
+        if p.returncode != 0:
+            result_status = "fail"
+            if "fatal: couldn't find remote ref" in p.stderr.decode("utf8"):
+                result["reason"] = "branch not found"
+        result["status"] = result_status
         return result
-
-def vector_validate_configs(paths):
-    p = subprocess.run([vector_bin_path, "validate"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if p.returncode == 0:
-        print(p.stdout)
-    print(p.returncode)
-
-def sync_branch(repo, branch, gitsync_root_path, git_sync_link_path):
-    #subprocess.run([gitsync_bin_path, "--repo", repo, "--ref", branch, "--root", gitsync_root_path, "--link", git_sync_link_path, "--one-time"])
-    
-    p = subprocess.run([gitsync_bin_path, "--repo", repo, "--ref", branch, "--root", gitsync_root_path, "--link", git_sync_link_path, "--one-time"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = {}
-    result_status = "ok"
-    if p.returncode != 0:
-        result_status = "fail"
-        if "fatal: couldn't find remote ref" in p.stderr.decode("utf8"):
-            result["reason"] = "branch not found"
-    result["status"] = result_status
-    return result
 
 def get_envvars(env_file='.env', set_environ=True, ignore_not_found_error=False, exclude_override=()):
     """
@@ -99,7 +159,7 @@ def load_envs(files):
         env_list = get_envvars(file, set_environ=False, ignore_not_found_error=True) 
         new_dict = {item['name']:item['value'] for item in env_list}
         result = result | new_dict
-        print ('new_dict',new_dict, result)
     return result
 
-print(vector_validate_config_branch(repo, "test-branch"))
+x = VectorAgent("/mnt/d/dev/github/vector-agent/app/config.yaml")
+print(x.validate_config_branch("test-branch"))
