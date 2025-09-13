b4009af9a1129c08bb52cf97ef8ad8ae3f8bf546
AlexeyNL <alexeynl@bk.ru>
2025-09-13 09:48:20 +0300

    make apply timeout configurable

--- app/utils.py
+++ app/utils.py
@@ -24,7 +24,7 @@ default_synced_config_dir =     "01-synced"
 default_hold_config_dir =       "02-hold"
 default_valid_config_dir =      "03-valid"
 default_active_config_dir =     "04-active"
-default_vector_reload_timeout = 60*2 #2 minutes
+default_apply_timeout = 60*2 #2 minutes
 default_vector_configs_workdir = "/opt/vector-agent/vector-confdir"
 default_apply_rules_config_name = "apply-rules.yaml"
 
@@ -40,7 +40,7 @@ class VectorAgent:
         self._vector_log_path = default_vector_log_path
         self._vector_embedded_config_dirs = default_vector_embedded_config_dirs
         self._gitsync_bin_path = default_gitsync_bin_path
-        self._vector_reload_timeout = default_vector_reload_timeout
+        self._apply_timeout = default_apply_timeout
         self._apply_rules_config_name = default_apply_rules_config_name
         self._vector_config_root_dir = None
         self._vector_config_subdir_patterns = None
@@ -86,7 +86,7 @@ class VectorAgent:
         logger.debug("_active_config_path = {}".format(self._active_config_path))
         logger.debug("_vector_bin_path = {}".format(self._vector_bin_path))
         logger.debug("_gitsync_bin_path = {}".format(self._gitsync_bin_path))
-        logger.debug("_vector_reload_timeout = {}".format(self._vector_reload_timeout))
+        logger.debug("_apply_timeout = {}".format(self._apply_timeout))
         logger.debug("_vector_log_path = {}".format(self._vector_bin_path))
         logger.debug("_apply_rules_config_name = {}".format(self._apply_rules_config_name))
         logger.debug("_apply_rules_config_path = {}".format(self._apply_rules_config_path))
@@ -181,6 +181,11 @@ class VectorAgent:
 
             try:
                 self._config_subdirs = data["vector-agent"]["config_subdirs"]
+            except KeyError:
+                pass
+
+            try:
+                self._apply_timeout = data["vector-agent"]["apply_timeout_sec"]
             except KeyError:
                 pass
 
@@ -440,7 +445,7 @@ class VectorAgent:
                     p = subprocess.run(["systemctl", "reload", "--quiet", self._vector_systemd_unit])
                     with open(self._vector_log_path, "r") as f:
                         logger.debug("Waiting for \"Vector has reloaded\" in Vector log")
-                        lines = follow(f, self._vector_reload_timeout)
+                        lines = follow(f, self._apply_timeout)
                         while (line := next(lines, None)) is not None:
                             if "Vector has reloaded" in line:
                                 vector_reload_success = True
@@ -466,7 +471,7 @@ class VectorAgent:
                         p = subprocess.run(["systemctl", "reload", "--quiet", self._vector_systemd_unit])
                         with open(self._vector_log_path, "r") as f:
                             logger.debug("Waiting for \"Vector has reloaded\" in Vector log")
-                            lines = follow(f, self._vector_reload_timeout)
+                            lines = follow(f, self._apply_timeout)
                             while (line := next(lines, None)) is not None:
                                 if "Vector has reloaded" in line:
                                     vector_reload_success = True
